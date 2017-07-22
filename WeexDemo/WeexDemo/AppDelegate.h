b63e9821a3405849f7e7f0adf51ad0cc9c63f9f1
JamesXuYTL <ytl_sdk@126.com>
2017-07-22 09:31:34 +0800

    初始化cocoapod工程

---
+++ WeexDemo/WeexDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeexDemo
+//
+//  Created by ZHG on 2017/7/22.
+//  Copyright © 2017年 XWJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeexDemo/WeexDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeexDemo
+//
+//  Created by ZHG on 2017/7/22.
+//  Copyright © 2017年 XWJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
